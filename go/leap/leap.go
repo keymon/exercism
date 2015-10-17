dbce2c6981631cca6fbf3911876dd1ef090ec8d9
Hector Rivas Gandara <keymon@gmail.com>
2015-10-17 01:12:57 +0100

    Implement go leap

---
+++ go/leap/leap.go
@@ -0,0 +1,9 @@
+package leap
+
+// Version of the matching exercism test
+const TestVersion = 1
+
+// IsLeapYear checks if the given year is a leap year
+func IsLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
